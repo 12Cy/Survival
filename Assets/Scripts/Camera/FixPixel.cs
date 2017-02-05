43c92807e460bf65ada2013a20b647c59a2c5bb0
12Cy <christian.sandkaemper@gmail.com>
2017-02-05 13:28:35 +0100

    asd

---
+++ Assets/Scripts/Camera/FixPixel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixPixel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public static Vector2 RoundToNearestPixel(Vector2 pos, Camera viewingCamera)
+
+    {
+
+        Vector2 valueInPixels = (Screen.height / (viewingCamera.orthographicSize * 2.0f)) * pos;
+
+        valueInPixels.x = Mathf.Round(valueInPixels.x);
+
+        valueInPixels.y = Mathf.Round(valueInPixels.y);
+
+
+        Vector2 adjustedUnityUnits = valueInPixels / (Screen.height / (viewingCamera.orthographicSize * 2.0f));
+
+        return adjustedUnityUnits;
+
+    }
+}
